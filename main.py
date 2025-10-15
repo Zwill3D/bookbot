95aa66f1e54904aa963a197cbb39473445835d65
Zwill3D <daviddrost@live.de>
2025-10-15 22:21:20 +0200

    dont even ask, lol, but its done

--- main.py
+++ main.py
@@ -3,21 +3,11 @@ from stats import (
     chars_dict_to_sorted_list,
     get_chars_dict,
 )
-
-
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print_report(book_path, num_words, chars_sorted_list)
-
+import sys
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
 
 def print_report(book_path, num_words, chars_sorted_list):
     print("============ BOOKBOT ============")
@@ -29,8 +19,18 @@ def print_report(book_path, num_words, chars_sorted_list):
         if not item["char"].isalpha():
             continue
         print(f"{item['char']}: {item['num']}")
-
     print("============= END ===============")
 
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path=sys.argv[1]
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 main()
